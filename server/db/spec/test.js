c460f6a1ce5f19aaa857d44c0debfc3e344fb97d
Justin Thareja <justin.thareja@gmail.com>
2015-06-25 19:31:30 -0700

    (feat) add getSkillId and getAllSkilId promises

--- server/db/spec/test.js
+++ server/db/spec/test.js
@@ -1,16 +1,18 @@
 var db = require('../config.js');
+var Promise = require('bluebird');
+var _ = require('underscore');
+var async = require('async');
 var Models = require('../models.js');
 var User = Models.User;
 var Offer = Models.Offer;
 var Want = Models.Want;
 
-
 var Collections = require('../collections.js');
-// var Offers = Collections.Offers;
-// var Users = Collections.Users;
-// var Wants = Collections.Wants;
+var Offers = Collections.Offers;
+var Users = Collections.Users;
+var Wants = Collections.Wants;
 
-var allOffers = Collections.allOffers;
+// var allOffers = Collections.allOffers;
 
 
 // // Query to add a new user with username and password:
@@ -77,7 +79,84 @@ var allOffers = Collections.allOffers;
 //   console.log('skill added');
 // });
 
-allOffers.create({skill: 'popping bubblewrap'});
+// var skill = 'sailing';
+
+// allOffers.create({skill: skill});
+
+// Offer.forge({ skill: skill }).fetch().then(function(skillExists) {
+//   if(!skillExists) {
+//     Offer.forge({ skill: skill }).then(function (skill) {
+//       console.log('skill added!');
+//     });
+//   }
+// });
+
+
+// getAllSkillIds accepts an array of skill strings, adds them to the db if not already there
+// and returns an array of id's associated with the corresponding skill
+// ex: getAllSkillIds(['skyrim', 'yoga', 'surfing']) => [4, 12, 6]
+
+var getAllSkillIds = function (skills) {
+  return new Promise (function (resolve, reject) {
+
+    var newSkillIds = [];
+    
+    async.each(skills, function (skill, callback) {
+
+      console.log('getting skill ID for', skill);
+      getSkillId(skill).then(function(skillId) {
+        newSkillIds.push(skillId);
+        callback();
+      });
+
+    }, function (err) {
+      if (err) {
+        console.log('failed to retreive skill id');
+      } else {
+        console.log('all new skills added to db');
+        resolve(newSkillIds);
+      }
+    });
+  });
+};
+
+
+
+// getSkillId is a promise that accepts a raw skill string (eg: 'cooking')
+// and passes it's corresponding id from the DB to the then function
+// if the skill is not already in the db, getSkillId will add it to the db before resolving
+var getSkillId = function (skill) {
+  return new Promise(function (resolve, reject) {
+    Offer.forge({ skill: skill }).fetch()
+      .then(function (skillExists) {
+        if (!skillExists) {
+          Offer.forge({ skill: skill}).save()
+            .then(function (savedSkill) {
+              console.log(savedSkill.get('skill'), 'saved successfully in offers table');
+              console.log('with an id of:', savedSkill.get('id'));
+              // pass saved skill id into then function
+              resolve(savedSkill.get('id'));
+            });
+        }
+        else {
+          console.log(skill, 'already exists in db');
+          // pass fetched skill id into then function
+          resolve(skillExists.get('id'));
+        }
+      });
+  });
+};
+
+getAllSkillIds(['snowboarding', 'reading', 'eating pizza']).then(function (ids) {
+  console.log('new skill ids from promise:', ids);
+});
+
+
+// then (new promiste)
+  // grab the array of related id's and save a new user by attaching the indicies to the appropriate table 
+  // (eg: 'wants', 'offers')
+  // v2 = modularize functionality to handle different charts
+
 
 
 
